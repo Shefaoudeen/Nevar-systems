24ecbf6e020ad68b82e4270968649e07daaa7003
Pradheep Raj <pradheepselva111@gmail.com>
2024-05-31 13:22:47 +0530

    client logo modified 2

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import logo1 from '../assets/clientLogos/logo1.png'
 import logo2 from '../assets/clientLogos/logo2.png'
 import logo3 from '../assets/clientLogos/logo3.png'
-import logo4 from '../assets/clientLogos/logo4.jpg'
+import logo4 from '../assets/clientLogos/logo4.png'
 import logo5 from '../assets/clientLogos/logo5.png'
 import logo6 from '../assets/clientLogos/logo6.png'
-import logo7 from '../assets/clientLogos/logo7.jpeg'
-import logo8 from '../assets/clientLogos/logo8.jpg'
+import logo7 from '../assets/clientLogos/logo7.png'
+import logo8 from '../assets/clientLogos/logo8.png'
 import logo9 from '../assets/clientLogos/logo9.png'
-import logo10 from '../assets/clientLogos/logo10.jpg'
+import logo10 from '../assets/clientLogos/logo10.png'
 import logo11 from '../assets/clientLogos/logo11.png'
 import logo12 from '../assets/clientLogos/logo12.png'
 import logo13 from '../assets/clientLogos/logo13.png'
-import logo14 from '../assets/clientLogos/logo14.jpg'
-import logo15 from '../assets/clientLogos/logo15.jpg'
+import logo14 from '../assets/clientLogos/logo14.png'
+import logo15 from '../assets/clientLogos/logo15.png'
 import logo16 from '../assets/clientLogos/logo16.jpeg'
 
 import gsap from "gsap";
@@ -57,14 +57,15 @@ const Client = () => {
   return (
     <div className="w-full h-screen bg-gray-900">
       {/* our clients */}
-      <div className="flex justify-center align items-center w-6/12 h-full">
-        {/* <h1 className="text-3xl translate-x-40 max-w-fit">Our Clients</h1> */}
+      <div className="relative flex  align items-center w-6/12 h-full">
+        
         {/* outer wrapper */}
-        <div className= "wrapper relative h-full w-[100vh] rounded-full -translate-x-80" style={{border: "15vh inset"}}> 
+          <h1 className="absolute z-50  left-[3%] text-3xl max-w-fit text-white">OUR CLIENTS</h1>
+        <div className= "wrapper flex justify-center items-center relative h-[90vh] w-[90vh] rounded-full  -translate-x-80" style={{boxShadow: "inset 0 0 0 15vh #D3D3D3"}}> 
               {logos.map((item)=>{
                 return (
-                <div style={{transform: `rotate(calc(360deg/16 *${item.index}))`,transformOrigin: "50vh"}} key={item.index} className=" absolute top-[50%]  bottom-[50%] left-[-20%] w-28 flex justify-center items-center">
-                  <img style={{transform: `rotate(calc(360deg/-16 *${item.index}))`}} src={item.img} className={`img w-24 p-1`}></img>
+                <div style={{transform: `rotate(calc(360deg/${logos.length} *${item.index}))`,transformOrigin: "45vh"}} key={item.index} className=" absolute z-10 top-[50%]  bottom-[50%] left-0 w-28 flex justify-center items-center">
+                  <img style={{transform: `rotate(calc(360deg/-${logos.length} *${item.index}))`}} src={item.img} className={`img w-24 p-1`}></img>
                   </div>
                 )
               })}
