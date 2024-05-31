e91d5452b6e6d4cd31f31b7dfe60faf162112fc4
Pradheep Raj <pradheepselva111@gmail.com>
2024-05-31 16:54:14 +0530

    added stats with counter animation

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import CountUp from 'react-countup';
+import gsap from "gsap";
+import { useGSAP, } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import logo1 from '../assets/clientLogos/logo1.png'
 import logo2 from '../assets/clientLogos/logo2.png'
 import logo3 from '../assets/clientLogos/logo3.png'
@@ -16,8 +21,6 @@ import logo14 from '../assets/clientLogos/logo14.png'
 import logo15 from '../assets/clientLogos/logo15.png'
 import logo16 from '../assets/clientLogos/logo16.jpeg'
 
-import gsap from "gsap";
-import { useGSAP } from '@gsap/react';
 const Client = () => {
 
   const logos = [
@@ -38,38 +41,73 @@ const Client = () => {
     {img: logo15, index : 14},
     {img: logo16, index : 15},
   ];
+  const wrapperRef = useRef();
+  const [statsInView,setStatsInView] = useState(false);
 
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(()=>{
+    wrapperRef.current.addEventListener('mouseover',(e)=>{
+      t1.pause();
+      t2.pause();
+    })
+    wrapperRef.current.addEventListener('mouseout',(e)=>{
+      t1.play();
+      t2.play();
+    })
+    gsap.to
+  },[])
+  const t1 = gsap.timeline();
+  const t2 = gsap.timeline();
   useGSAP(()=>{
-    gsap.to('.wrapper',{
+    t1.to(wrapperRef.current,{
       rotate : "360deg",
       duration : 25,
       repeat : -1,
       ease : "none"
     })
-    gsap.to('.img',{
+    t2.to('.img',{
       rotate : "-=360deg",
       duration : 25,
       repeat : -1,
       ease : "none"
     })
+    gsap.to('.stats',{
+      scrollTrigger : {
+        trigger : '.stats',
+        start : "top bottom",
+        end : "bottom top",
+        onToggle : ({isActive}) => setStatsInView(true),
+      }
+    })
   },[])
-
   return (
-    <div className="w-full h-screen bg-gray-900">
+    <div className="w-full flex h-screen bg-gray-900">
       {/* our clients */}
-      <div className="relative flex  align items-center w-6/12 h-full">
+      <div className="hidden xl:flex relative items-center w-[45vw] h-full">
         
         {/* outer wrapper */}
-          <h1 className="absolute z-50  left-[3%] text-3xl max-w-fit text-white">OUR CLIENTS</h1>
-        <div className= "wrapper flex justify-center items-center relative h-[90vh] w-[90vh] rounded-full  -translate-x-80" style={{boxShadow: "inset 0 0 0 15vh #D3D3D3"}}> 
+      <div className= "hidden xl:flex justify-center items-center relative h-[45vw] w-[45vw] -translate-x-80">
+          <h1 className="absolute right-[15vw] text-[2vw] w-20 text-white font-bold">OUR CLIENTS</h1>
+        <div ref={wrapperRef} className= "wrapper flex  items-center relative h-full w-full" style={{boxShadow: "inset 0 0 0 15vh #D3D3D3",borderRadius:"50%"}}> 
               {logos.map((item)=>{
                 return (
-                <div style={{transform: `rotate(calc(360deg/${logos.length} *${item.index}))`,transformOrigin: "45vh"}} key={item.index} className=" absolute z-10 top-[50%]  bottom-[50%] left-0 w-28 flex justify-center items-center">
+                <div style={{transform: `rotate(calc(360deg/${logos.length} *${item.index}))`,transformOrigin: "22.5vw"}} key={item.index} className=" absolute z-10 top-[50%]  bottom-[50%] left-0 w-28 flex justify-center items-center">
                   <img style={{transform: `rotate(calc(360deg/-${logos.length} *${item.index}))`}} src={item.img} className={`img w-24 p-1`}></img>
                   </div>
                 )
               })}
         </div>
+      </div>
+      </div>
+      {/* stats and partners*/}
+      <div className="flex h-full">
+        {/* stats */}
+        <div className="flex justify-between text-white text-6xl mt-24 stats">
+              <h1 className="">{statsInView && <CountUp start={0} end={55} duration={4}></CountUp>}+ Clients</h1>
+              <h1 className="">{statsInView &&<CountUp start={0} end={8000} duration={4}></CountUp>}+ sqkm</h1>
+              <h1 className="">{statsInView &&<CountUp start={0} end={70} duration={4}></CountUp>}+ Projects</h1>
+        </div>
+            
       </div>
     </div>
   );
